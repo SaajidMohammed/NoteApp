d94d176f21443f4e5a13681b34ee7f8b03a1bb95
SaajidMohammed <saajidmohammed6@gmail.com>
2026-01-31 11:17:55 +0530

    Backend URL Added

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://noteappbackend-6o6u.onrender.com/api',
 });
 
 // Interceptor to add Authorization header
