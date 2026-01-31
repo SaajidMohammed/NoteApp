c9f521ea3f09f9fb5136fa8d6128a93415e5b2d2
SaajidMohammed <saajidmohammed6@gmail.com>
2026-01-31 11:51:22 +0530

    Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import { Toaster } from 'react-hot-toast';
 
 // Pages
 import Login from './pages/Login';
-import Register from './pages/register';
+import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 
 // Components
